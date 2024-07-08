085cd8939d851f1240bc93c4d9f81a4ed3435d2f
Paul Tapping <zombieplayground@gmail.com>
2024-07-08 19:38:28 +0100

    BUILD: 'edit categories' functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -21,3 +21,12 @@ def add_categories():
       return redirect(url_for("categories"))  
     return render_template("add_categories.html")
 
+@app.route("/edit_category/<int:category_id>", methods=["GET","POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_categories.html", category=category)
+
