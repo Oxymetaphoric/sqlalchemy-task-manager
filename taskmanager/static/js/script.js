523aa25eae5233aa521fb159f92ea9267ad07e30
Paul Tapping <zombieplayground@gmail.com>
2024-07-09 16:39:04 +0100

    BUILD: Add Task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,16 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {
+      format: "dd mmm yyyy",
+      i18n: {done: "select"}
+    });
+  });
+
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
